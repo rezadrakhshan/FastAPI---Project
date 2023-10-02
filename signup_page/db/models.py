a4f59a993646a01e2ca157c0350bab5f58288bcd
RezaDerakhshan <130975344+rezadrakhshan@users.noreply.github.com>
2023-10-02 16:38:18 +0330

    Delete signup_page directory

--- signup_page/db/models.py
+++
@@ -1,9 +0,0 @@
-from db.datebase import Base
-from sqlalchemy import String,Integer,Column
-
-class User(Base):
-    __tablename__ = "users"
-    name = Column(String)
-    password = Column(Integer)
-    email = Column(String,primary_key=True)
-
